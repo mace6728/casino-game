c909b8437ac74f7ad366cff2db4bdde762c5a99f
euphsiang <euphsiang040304@gmail.com>
2024-12-11 16:56:25 +0800

    Add files via upload

---
+++ lobby/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Casino</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="image-container" style="left: 15%; bottom: 5%;">
+    <a href="../Sic_Bo/index.php"><img src="SicBo.png" alt="SicBo" class="hover-image"></a>
+  </div>
+  <div class="image-container" style="left: 15%; top: 5%;">
+    <a href="../Roulette/index.php"><img src="Roulette.png" alt="Roulette" class="hover-image"></a>
+  </div>
+  <div class="image-container" style="right: 15%; top: 5%;">
+    <a href="../Baccarat/baccarat.html"><img src="Baccarat_logo 1.png" alt="Baccarat" class="hover-image"></a>
+  </div>
+  <div class="image-container" style="right: 15%; bottom: 3%;">
+    <button><img src="blackjack.png" alt="blackjack" class="hover-image"></button>
+  </div>
+</body>
+</html>
