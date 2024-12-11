4d62910a0062c545653c14decd59e4b8d64764c1
chiafu2018 <chiafu2018@gmail.com>
2024-12-11 16:46:04 +0800

    CI: add users table

--- Baccarat/baccarat.php
+++ Baccarat/baccarat.php
@@ -1,7 +1,9 @@
 <?php
-    $dbname = 'your_database_name'; 
-    $dbusername = 'your_database_username'; 
-    $dbpassword = 'your_database_password'; 
+    $dbname = 'casino'; 
+    $dbusername = 'root'; 
+    $dbpassword = '';
+
+    // table name i'm gonna to access is called users
 
     try {
         $database = new PDO("mysql:host=localhost;dbname=$dbname", $dbusername, $dbpassword);
@@ -13,7 +15,7 @@
     if(array_key_exists('username', $_GET)){
         $username = decodeURIComponent($_GET['username']);
         try {
-            $query = "SELECT money FROM login WHERE username = :username";
+            $query = "SELECT money FROM users WHERE username = :username";
             $stmt = $database->prepare($query);
             $stmt->bindParam(':username', $username, PDO::PARAM_STR);
             $stmt->execute();
@@ -34,7 +36,7 @@
         $bank = intval(decodeURIComponent($_POST['bank']));
 
         try {
-            $updateQuery = "UPDATE login SET money = :newBankAmount WHERE username = :username";
+            $updateQuery = "UPDATE users SET money = :newBankAmount WHERE username = :username";
             $updateStmt = $database->prepare($updateQuery);
             $updateStmt->bindParam(':newBankAmount', $bank, PDO::PARAM_INT);
             $updateStmt->bindParam(':username', $username, PDO::PARAM_STR);
---
+++ lobby/users.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2024-12-11 09:44:58
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `casino`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `users`
+--
+
+CREATE TABLE `users` (
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `money` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `money`) VALUES
+('jeff', '12345', 2000);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
