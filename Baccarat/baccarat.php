c93cfb29e4a6b43a255826a63f40fdc2580a58e4
chiafu2018 <chiafu2018@gmail.com>
2024-12-12 02:42:37 +0800

    CI: Barccarat link to database

--- Baccarat/baccarat.php
+++ Baccarat/baccarat.php
@@ -13,30 +13,31 @@
     }
 
     if(array_key_exists('username', $_GET)){
-        $username = decodeURIComponent($_GET['username']);
+        $username = $_GET['username'];
         try {
-            $query = "SELECT money FROM users WHERE username = :username";
+            $query = "SELECT chips FROM users WHERE username = :username";
             $stmt = $database->prepare($query);
             $stmt->bindParam(':username', $username, PDO::PARAM_STR);
             $stmt->execute();
 
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
             if ($result) {
-                echo $result['money']; 
+                echo json_encode(['bank' => $result['chips']]);
             } else {
-                echo "0";
+                echo json_encode(['bank' => 0]);
             }
+
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
         }
     }
 
     if(array_key_exists('username', $_POST) && array_key_exists('bank', $_POST)){
-        $username = decodeURIComponent($_POST['username']);
-        $bank = intval(decodeURIComponent($_POST['bank']));
+        $username = $_POST['username'];
+        $bank = intval($_POST['bank']);
 
         try {
-            $updateQuery = "UPDATE users SET money = :newBankAmount WHERE username = :username";
+            $updateQuery = "UPDATE users SET chips = :newBankAmount WHERE username = :username";
             $updateStmt = $database->prepare($updateQuery);
             $updateStmt->bindParam(':newBankAmount', $bank, PDO::PARAM_INT);
             $updateStmt->bindParam(':username', $username, PDO::PARAM_STR);
