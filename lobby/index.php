f06662880ffd1750679eaeb3f583c6e14e2bada2
euphsiang <euphsiang040304@gmail.com>
2024-12-12 14:22:46 +0800

    lobby layout

---
+++ lobby/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Casino</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Remove redundant or conflicting stylesheets -->
+  <!-- <link rel="stylesheet" href="../Roulette/style.css"> -->
+</head>
+<body>
+  <!-- Navigation Bar -->
+  <nav class="navbar">
+    <ul>
+      <li><a href="index.php"><i class="fa fa-home"></i> Lobby</a></li>
+      <li><a href="../Roulette/login.html"><i class="fa fa-sign-in"></i> Login</a></li>
+      <li><a href="../Roulette/register.html"><i class="fa fa-user-plus"></i> Register</a></li>
+      <li><a href="../lobby/index.php"><i class="fa fa-sign-out"></i> Logout</a></li>
+    </ul>
+  </nav>
+
+  <!-- Main Content -->
+  <!-- <h1>Online Casino</h1>  -->
+  <img src="casino-background-Photoroom.png" style="text-align: center; width: 25%;">
+
+  <div class="side-decoration left">♠️ ♥️ ♣️ ♦️ ♠️ ♥️ ♣️</div>
+  <div class="side-decoration right">♦️ ♣️ ♥️ ♠️ ♦️ ♣️ ♥️</div>
+
+  <div class="separator-line"></div>
+
+  <div class="games-container">
+    <div class="image-container">
+      <a href="../Sic_Bo/index.php">
+        <img src="SicBo.png" alt="SicBo" class="hover-image">
+      </a>
+    </div>
+
+    <div class="image-container">
+      <a href="../Roulette/index.php">
+        <img src="Roulette.png" alt="Roulette" class="hover-image">
+      </a>
+    </div>
+
+    <div class="image-container">
+      <a href="../Baccarat/baccarat.html">
+        <img src="Baccarat_logo1.png" alt="Baccarat" class="hover-image">
+      </a>
+    </div>
+
+    <div class="image-container">
+      <a href="../Blackjack/index.html">
+        <img src="blackjack.png" alt="Baccarat" class="hover-image">
+      </a>
+    </div>
+  </div>
+
+</body>
+<footer class="footer">
+  <p>© 2024 Casino Games, All rights reserved.</p>
+</footer>
+</html>
